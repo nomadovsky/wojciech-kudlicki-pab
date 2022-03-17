00678ac329c38f8479e18b59926a2ea6bbf6b245
nomadovsky <nomadovsky@gmail.com>
2022-03-17 12:04:28 +0100

    add delete note

--- projekt-2/src/index.ts
+++ projekt-2/src/index.ts
@@ -2,9 +2,8 @@ import express from "express";
 import { Request, Response } from "express";
 import { Note } from "./note";
 
-const app = express();
-
-const notes: Note[] = [];
+// Notes
+let notes: Note[] = [];
 
 function createNote(title: string, content: string, tags?: string[]): Note {
   const date = new Date(Date.now());
@@ -14,21 +13,41 @@ function createNote(title: string, content: string, tags?: string[]): Note {
 }
 
 // Routing
+const app = express();
+
 app.use(express.json());
 
-app.get("/note/:id", function (req: Request, res: Response) {
-  //Zwraca kod 200 i JSON z notatką lub 404 jeśli notatka nie istnieje
-  const note = notes.find((n) => (n.id = +req.params.id));
-  const noteJSON = JSON.stringify(note);
-  if (res.status(200)) res.send(`200 ${noteJSON}`);
-  else res.sendStatus(404);
-});
 app.post("/note", function (req: Request, res: Response) {
   const note = createNote("New Title1", "Content1");
   notes.push(note);
-  console.log(notes);
   if (res.status(201)) res.send(`201 Note ID: ${note.id}`);
   else res.sendStatus(400);
 });
 
+app.get("/note/:id", function (req: Request, res: Response) {
+  //Zwraca kod 200 i JSON z notatką lub 404 jeśli notatka nie istnieje
+  const note = notes.find((n) => n.id === +req.params.id);
+  const noteJSON = JSON.stringify(note);
+
+  if (note) res.status(200).send(`200 ${noteJSON}`);
+  else res.sendStatus(404);
+});
+app.get("/notes", function (req: Request, res: Response) {
+  res.send(notes);
+});
+
+app.put("/note/:id", function (req: Request, res: Response) {
+  const note = notes.find((n) => n.id === +req.params.id);
+  const noteJSON = JSON.stringify(note);
+  if (res.status(200)) res.send(note);
+  else res.sendStatus(404);
+});
+
+app.delete("/note/:id", function (req: Request, res: Response) {
+  const note = notes.find((n: Note) => n.id === +req.params.id);
+  if (note) {
+    notes = notes.filter((n: Note) => n.id !== +req.params.id);
+    res.sendStatus(204);
+  } else res.sendStatus(400);
+});
 app.listen(3000);
