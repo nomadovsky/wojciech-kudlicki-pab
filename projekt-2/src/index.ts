a6d0af74fc9ef0592585b1bf6c8d987927aaf126
nomadovsky <nomadovsky@gmail.com>
2022-03-17 10:11:04 +0100

    add get note

--- projekt-2/src/index.ts
+++ projekt-2/src/index.ts
@@ -16,14 +16,19 @@ function createNote(title: string, content: string, tags?: string[]): Note {
 // Routing
 app.use(express.json());
 
-app.get("/", function (req: Request, res: Response) {
-  res.status(200).send("Get");
+app.get("/note/:id", function (req: Request, res: Response) {
+  //Zwraca kod 200 i JSON z notatką lub 404 jeśli notatka nie istnieje
+  const note = notes.find((n) => (n.id = +req.params.id));
+  const noteJSON = JSON.stringify(note);
+  if (res.status(200)) res.send(`200 ${noteJSON}`);
+  else res.sendStatus(404);
 });
 app.post("/note", function (req: Request, res: Response) {
   const note = createNote("New Title1", "Content1");
   notes.push(note);
   console.log(notes);
-  res.status(200).send(`Posted. Number of notes: ${notes.length}`);
+  if (res.status(201)) res.send(`201 Note ID: ${note.id}`);
+  else res.sendStatus(400);
 });
 
 app.listen(3000);
